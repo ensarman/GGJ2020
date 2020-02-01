dc5a70a68c160383ca41eb1d1104f31f4f924f13
darkfenix <rely.m.v@gmail.com>
2020-02-01 11:04:23 -0500

    agrego

--- puente.gd
+++ puente.gd
@@ -1,11 +1,25 @@
 extends Node2D
 var time_generate_new_oponent = 100 #esto tendria que se aleatorio dependiendo del nivel
 const MySmokeResource = preload("res://Auto.tscn")
-var damage_temp = 10
+var damage_temp:int = 10
 var GrabedInstance
-var type_car = 0
-var speed = 200
-var damage = 10
+var type_car:int = 0
+var speed :int = 200
+var damage:int = 10
+var texture:Texture 
+
+var bus = preload("res://sprite/bus.png")
+var camioneta = preload("res://sprite/camioneta.png")
+var combi = preload("res://sprite/combi.png")
+var compacto = preload("res://sprite/compacto.png")
+var convertible = preload("res://sprite/convertible.png")
+var furgon = preload("res://sprite/furgon.png")
+var hatchbag = preload("res://sprite/hatchbag.png")
+var micro = preload("res://sprite/micro.png")
+var sedanantiguo = preload("res://sprite/sedanantiguo.png")
+var smart = preload("res://sprite/smart.png")
+var stationwagon = preload("res://sprite/stationwagon.png")
+
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -14,10 +28,13 @@ var damage = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	GrabedInstance= MySmokeResource.instance()
 	GrabedInstance.global_position = $SpawAutos.position
 	GrabedInstance.speed = 200
 	GrabedInstance.damage = 300
+	GrabedInstance.type_car = type_car
+	GrabedInstance.texture = bus
 	self.add_child(GrabedInstance)
 	pass # Replace with function body.
 
@@ -37,31 +54,47 @@ func _on_Timer_timeout():
 	pass # Replace with function body.
 
 func _create_car():
+	
 	GrabedInstance.global_position = $SpawAutos.position
 	type_car= randi()%6
 	if type_car == 0:
 		speed = 200
 		damage = 100
+		texture = convertible
 	elif type_car == 1:
 		speed = 180
 		damage = 200
+		texture=compacto
 	elif type_car == 2:
 		speed = 150
 		damage = 300
+		texture=hatchbag
 	elif type_car == 3:
 		speed = 110
 		damage = 400
+		texture=camioneta
 	elif type_car == 4:
 		speed = 100
 		damage = 500
+		texture=combi
+	elif type_car == 5:
+		speed = 100
+		damage = 500
+		texture=stationwagon
+	elif type_car == 6:
+		speed = 100
+		damage = 500
+		texture=bus
 	else:
 		speed = 90
 		damage = 700
+		texture=furgon
 	
 	 
 	GrabedInstance.speed = speed
 	GrabedInstance.damage = damage
 	GrabedInstance.type_car = type_car
+	GrabedInstance.texture = texture
 	self.add_child(GrabedInstance)
 	pass
 
