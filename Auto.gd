1f0c08de55d603474dd2d287ad35dead79a91210
fenix <rely.m.v@gmail.com>
2020-01-31 21:14:40 -0500

    inicial

---
+++ Auto.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var velocidad = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	velocidad.x = 0
+	velocidad.y = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	pass
