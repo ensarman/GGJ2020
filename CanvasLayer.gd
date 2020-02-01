d57c5b6d20f91598e37dfe48ba7c419f04fd1580
darkfenix <rely.m.v@gmail.com>
2020-02-01 11:47:25 -0500

    mejora paralax

--- CanvasLayer.gd
+++ CanvasLayer.gd
@@ -5,6 +5,8 @@ extends CanvasLayer
 # var a = 2
 # var b = "text"
 var offset1 = Vector2()
+var offset2 = Vector2()
+var offset3 = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,4 +17,8 @@ func _ready():
 func _process(delta):
 	offset1.x -= 10 * delta
 	$ParallaxBackground/ParallaxLayer.set_motion_offset(offset1)
+	offset2.x -= 30 * delta
+	$ParallaxBackground/ParallaxLayer2.set_motion_offset(offset2)
+	offset3.x -= 40 * delta
+	$ParallaxBackground/ParallaxLayer3.set_motion_offset(offset3)
 	pass
